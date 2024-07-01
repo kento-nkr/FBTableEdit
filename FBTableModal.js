81ab1e30dbd4691a6d4deb540dbf92d61cc53dc7
Kento Tokura <k-tokura@nkr-group.com>
2024-07-01 12:11:06 +0900

    1.1

--- FBTableModal.js
+++ FBTableModal.js
@@ -40,11 +40,11 @@ class FBTableModal {
   }
 
   #completeInit() {
-    //    this.table_values[0].forEach((elem) => {
-    //      let write_elem = elem;
-    //      write_elem.value = "";
-    //      this.template_row_value.push(write_elem);
-    //    });
+    this.table_values[0].forEach((elem) => {
+      let write_elem = elem;
+      //      write_elem.value = "";
+      this.template_row_value.push(write_elem);
+    });
     if (this.table_dom == undefined)
       console.error(
         `fieldcode=${table_fieldcode} is not defined as Table element. Please check your FB setting.`
